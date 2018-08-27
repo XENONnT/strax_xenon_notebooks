5c9ac88c797a84cf486e14c0116f637179678a16
Chris Tunnell <2406627+tunnell@users.noreply.github.com>
2018-08-27 11:26:22 +0200

    Update basic_context_setup.py

--- basic_context_setup.py
+++ basic_context_setup.py
@@ -11,5 +11,5 @@ st = strax.Context(register_all=strax.xenon.plugins,
 
 # This is needed since data came from XENON1T (you don't need pax)
 st.register(strax.xenon.pax_interface.RecordsFromPax)
-
-print(st.get_df('170319_0510', 'event_info').head())
+dataset = '170319_0510'
+print(st.get_df(dataset, 'event_info').head())
